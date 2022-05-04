559e118671321b4a6e74f4363d17928c3916509f
unknown <krystalnickole_12@yahoo.com>
2022-05-03 20:20:16 -0500

    associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+class Product extends Model {
+  static associate({Category}) {
+    // define association here
+    this.belongsTo(Category, {foreignKey: 'category_id', as: 'category' })
+  }
+};
 // Categories have many Products
-
+class Category extends Model {
+  static associate({Product}) {
+    // define association here
+    this.hasMany(Product, {foreignKey: 'category_id', as: 'products' })
+  }
+};
 // Products belongToMany Tags (through ProductTag)
+class Product extends Model {
+  static associate({Tags}) {
+    // define association here
+    this.belongsTo(Category, { through: ProductTag })
+  }
+};
 
 // Tags belongToMany Products (through ProductTag)
+class Tag extends Model {
+  static associate({Product}) {
+    // define association here
+    this.hasMany(Product, {foreignKey: 'category_id', as: 'products' })
+  }
+};
 
 module.exports = {
   Product,
